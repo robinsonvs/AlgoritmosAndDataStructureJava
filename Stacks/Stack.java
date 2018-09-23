fdd740511219658641abcacf99f823ab65822a44
Robinson Severo <robinsonvs@gmail.com>
2018-09-23 20:18:24 -0300

    first commit 1

---
+++ Stacks/Stack.java
@@ -0,0 +1,23 @@
+
+public class Stack {
+
+    public Stack(){
+
+    }
+
+    public void push(int element) {
+        
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public boolean isFull() {
+
+    }
+
+    public void pop() {
+
+    }
+}
